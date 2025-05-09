a7f325ee929a469422e993218b79bf520c28c4bf
HosseinRezaeiFard <huseinrezaeif99@gmail.com>
2025-05-09 12:16:20 -0700

    Implement price converter lib

--- FundMe.sol
+++ FundMe.sol
@@ -2,26 +2,23 @@
 
 pragma solidity 0.8.30;
 
-import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import { PriceConverter } from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 public minUsd = 5e18;
 
+    address[] public funders;
+
+    mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
+
     function fund() public payable {
-        require(getConversionRate(msg.value) > minUsd, "Didn't sent enough ETH");
+        require(msg.value.getConversionRate() > minUsd, "Didn't sent enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
     function withdraw() public {}
-
-    function getEthPrice() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        return uint256(price * 1e10);
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getEthPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
+    
 }
