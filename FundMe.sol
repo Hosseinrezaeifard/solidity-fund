1f88c2dfd4bf7f0c2c10c8f39ed9dc63615a98e7
HosseinRezaeiFard <huseinrezaeif99@gmail.com>
2025-05-09 13:45:47 -0700

    Implement fallback & receive function

--- FundMe.sol
+++ FundMe.sol
@@ -57,5 +57,21 @@ contract FundMe {
         */
         _;
     }
+
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback()
+    //     /   \
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+    
+    receive() external payable { fund(); }
+
+    fallback() external payable { fund(); }
     
 }
